f13e5800113086c79bd717d25722bc9e385887a6
Bhikule19 <108789846+Bhikule19@users.noreply.github.com>
2022-10-27 09:37:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// for (i = 0; i < 2; i++) {
+//     for (var j = 0; j < 3; j++) {
+//         console.log("Hello");
+//     }
+// }
+for (var j = 0; j < 3; j++) {
+    console.log("Hello");
+}
+/*
+Comment 4
+*/
+let a = prompt("Enter a no.")
